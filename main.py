068c9a30959f578037247d892e17506a0467aa79
ericmartinezr <eric.martinez@live.cl>
2026-04-05 12:00:51 -0400

    wip

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import requests
 from dotenv import load_dotenv
+from pyspark.sql import SparkSession
+from datetime import datetime
+
 
 load_dotenv()
 
@@ -8,18 +11,21 @@ load_dotenv()
 
 URL = "https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={api_key}"
 
+spark = SparkSession.builder.appName("NASA Asteroids").getOrCreate()
+
 
 def just_print_response(response):
     print(response)
 
 
 def silver_response(response):
-    asteroids = {}
+    asteroids = []
+    today = datetime.today().strftime('%Y-%m-%d')
     for date, asteroid_list in response["near_earth_objects"].items():
-        asteroids[date] = []
         for asteroid in asteroid_list:
             obj_asteroid = {
                 "name": asteroid["name"],
+                "process_date": today,
                 "data_date": date,
                 "is_potentially_hazardous": asteroid["is_potentially_hazardous_asteroid"],
                 "estimated_diameter_km_min": asteroid["estimated_diameter"]["kilometers"]["estimated_diameter_min"],
@@ -36,9 +42,13 @@ def silver_response(response):
             obj_asteroid["is_fast"] = float(
                 obj_asteroid["kms_per_second"]) > 10
 
-            asteroids[date].append(obj_asteroid)
+            asteroids.append(obj_asteroid)
 
-    print(asteroids)
+    df = spark.createDataFrame(asteroids)
+    df.write.mode("overwrite").parquet(
+        path=f"bronze/{today}/asteroids.parquet",
+        partitionBy=["process_date"]
+    )
 
 
 def gold_response(response):
